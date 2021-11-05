80aa162d8da54a2ba6311e1bdf9cb9b06b57a987
Antonio Cabezuelo Vivo <antonio@tapsandswipes.com>
2021-11-05 19:53:37 +0100

    Add async version of run
    Signed-off-by: Antonio Cabezuelo Vivo <antonio@tapsandswipes.com>

---
+++ Sources/FutureResult/FutureResult+Async.swift
@@ -0,0 +1,44 @@
+//
+//  File.swift
+//  
+//
+//  Created by Antonio Cabezuelo Vivo on 5/11/21.
+//
+
+import Foundation
+
+#if swift(>=5.5.2)
+
+@available(iOS 13.0, macOS 10.15, tvOS 13.0, watchOS 6.0, *)
+public
+extension FutureResult {
+    func run() async throws -> R {
+        try await withCheckedThrowingContinuation { self.run($0.resume) }
+    }
+}
+
+@available(iOS 13.0, macOS 10.15, tvOS 13.0, watchOS 6.0, *)
+public
+extension FutureResult where R == Void {
+    func run() async throws {
+        _ = try await withCheckedThrowingContinuation { self.run($0.resume) }
+    }
+}
+
+@available(iOS 13.0, macOS 10.15, tvOS 13.0, watchOS 6.0, *)
+public
+extension FutureResult where E == Never {
+    func run() async -> R {
+        await withCheckedContinuation { self.run($0.resume) }
+    }
+}
+
+@available(iOS 13.0, macOS 10.15, tvOS 13.0, watchOS 6.0, *)
+public
+extension FutureResult where R == Void, E == Never {
+    func run() async {
+        _ = await withCheckedContinuation { c in self.run { _ in c.resume() } }
+    }
+}
+
+#endif
